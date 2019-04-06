e3a304c346876d831614139c0dffd86e9fd2758e
Lucranix28 <45935449+Lucranix28@users.noreply.github.com>
2019-04-06 18:01:33 +0700

    Create tes_F13_user.pas

---
+++ tes_F13_user.pas
@@ -0,0 +1,12 @@
+procedure load;
+type
+  User = record
+    Nama, Alamat, Username, Password : string;
+    Role : string[Pengunjung, Admin];
+  end;
+
+  var
+    user = array of User;
+  begin
+
+  end;
