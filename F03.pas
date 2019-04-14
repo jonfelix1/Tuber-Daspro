fb19135c79b2ffc42fb50f9f273c573fc5acb253
jonfelix1 <49271261+jonfelix1@users.noreply.github.com>
2019-04-14 20:16:10 +0700

    Update F03.pas

--- F03.pas
+++ F03.pas
@@ -1,76 +1,88 @@
 program F03;
-uses db, sysutils;
+
+//Kamus
 
 const
     nmax = 100;
 
 type
     buku = record
-        id: longint;
-        judul : string;
-        penulis: string;
+        id_buku: longint;
+        judul_buku : string;
+        author: string;
         kategori: string;
-        tahun : integer;
-   
-end;
+        tahun_terbit : integer;
+    end;
 
 var
     list : array [1..nmax] of buku;
     i,result : integer;
     input : string;
+    stop : boolean;
+
+//Prosedur output buku
 
 procedure print_buku( var bukua: buku);
     begin
-        write(bukua.id, ' | ');
-        write(bukua.judul, ' | ');
-        write(bukua.penulis);
-        writeln();
+    write(bukua.id_buku, ' | ');
+    write(bukua.judul_buku, ' | ');
+    write(bukua.author);
+    writeln();
     end;
+
+//Algoritma
 
 begin
-    {Ganti ke csv}
-    with list[1] do
+{Ganti ke csv}
+stop := false;
+with list[1] do
     begin
-        id := 2516;
-        judul := 'Clean Code';
-        penulis := 'Robert';
-        kategori := 'programming';
-        tahun := 2010;
+    id_buku := 2516;
+    judul_buku := 'Clean Code';
+    author := 'Robert';
+    kategori := 'programming';
+    tahun_terbit := 2010;
     end;
     
-    with list[2] do
+with list[2] do
     begin
-        id := 786727;
-        judul := 'Competitive Programming 3';
-        penulis := 'Steven';
-        kategori := 'programming';
-        tahun := 2010;
+    id_buku := 786727;
+    judul_buku := 'Competitive Programming 3';
+    author := 'Steven';
+    kategori := 'programming';
+    tahun_terbit := 2010;
     end;
 
-    result := 0;
+result := 0;
 
-    while True do
+while not(stop) do
+    begin
+    write('Masukan kategori: ');
+    readln(input);
+    if  (input = 'sastra') or
+        (input = 'sains') or
+        (input = 'manga')  or
+        (input = 'sejarah')  or
+        (input = 'programming') then
         begin
-            write('Masukan kategori: ');
-            read(input);
-            if  (input = 'sastra') or
-                (input = 'sains') or
-                (input = 'manga')  or
-                (input = 'sejarah')  or
-                (input = 'programming') then
-                    begin
-                        writeln('Hasil pencarian :');
-                        for i := 1 to nmax do 
-                            begin
-                                if list[i].kategori = input then
-                                    begin
-                                        print_buku(list[i]);
-                                        result += 1;
-                                    end;
-                            end;
-                        if result = 0 then writeln('Tidak ada buku dalam kategori ini.');
-                        exit;
-                    end
-            else writeln('Kategori ', input, ' tidak valid.');
-        end;
+        writeln('Hasil pencarian :');
+        for i := 1 to nmax do 
+            begin
+            if list[i].kategori = input then
+                begin
+                print_buku(list[i]);
+                result += 1;
+                end;
+            end;
+    if result = 0 then 
+        begin
+        writeln('Tidak ada buku dalam kategori ini.');
+        stop := True;
+        end
+    else
+        stop := True;
+    
+    end
+    else writeln('Kategori ', input, ' tidak valid.');
+    end;
 end.
