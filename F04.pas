7c27890f3b89f9b7befea5874c48ed85bb1008bf
jonfelix1 <49271261+jonfelix1@users.noreply.github.com>
2019-04-14 20:31:22 +0700

    Update F04.pas

--- F04.pas
+++ F04.pas
@@ -1,121 +1,138 @@
 program F04;
-uses db, sysutils;
+
+//Kamus Lokal
 
 const
     nmax = 100;
 
 type
     buku = record
-        id: longint;
-        judul : string;
-        penulis: string;
+        id_buku: longint;
+        judul_buku : string;
+        author: string;
         kategori: string;
-        tahun : integer;
-   
-end;
+        tahun_terbit : integer;
+        jumlah_buku : integer;
+    end;
 
 var
     list : array [1..nmax] of buku;
     i, tahuna, result : integer;
     kategori : string;
+    stop : boolean;
+
+//Prosedur print buku
 
 procedure print_buku( var bukua: buku);
     begin
-        write(bukua.id, ' | ');
-        write(bukua.judul, ' | ');
-        write(bukua.penulis);
-        writeln();
+    write(bukua.id_buku, ' | ');
+    write(bukua.judul_buku, ' | ');
+    write(bukua.author);
+    writeln();
     end;
+
+//Algoritma
 
 begin
-    {Ganti ke csv}
-    with list[1] do
+stop := false;
+
+with list[1] do
     begin
-        id := 2516;
-        judul := 'Clean Code';
-        penulis := 'Robert';
-        kategori := 'programming';
-        tahun := 2010;
+    id_buku := 2516;
+    judul_buku := 'Clean Code';
+    author := 'Robert';
+    kategori := 'programming';
+    tahun_terbit := 2010;
     end;
     
-    with list[2] do
+with list[2] do
     begin
-        id := 786727;
-        judul := 'Competitive Programming 3';
-        penulis := 'Steven';
-        kategori := 'programming';
-        tahun := 2011;
+    id_buku := 786727;
+    judul_buku := 'Competitive Programming 3';
+    author := 'Steven';
+    kategori := 'programming';
+    tahun_terbit := 2011;
     end;
 
-    result := 0;
+result := 0;
 
-    while True do
+while not(stop) do
+    begin
+    write ('Masukan tahun_terbit: ');
+    readln(tahuna);
+    write ('Masukan kategori: ');
+    readln(kategori);
+    writeln();
+    writeln('Buku yang terbit ', kategori, ' ', tahuna, ': ');
+    if kategori = '=' then
         begin
-            write ('Masukan tahun: ');
-            readln(tahuna);
-            write ('Masukan kategori: ');
-            readln(kategori);
-            writeln();
-            writeln('Buku yang terbit ', kategori, ' ', tahuna, ': ');
-            if kategori = '=' then
+        for i := 1 to nmax do 
+            begin
+            if list[i].tahun_terbit = tahuna then
                 begin
-                    for i := 1 to nmax do 
-                            begin
-                                if list[i].tahun = tahuna then
-                                    begin
-                                        print_buku(list[i]);
-                                        result += 1;
-                                    end;
-                            end;
-                        if result = 0 then writeln('Tidak ada buku dalam kategori ini.');
-                end
-            else if kategori = '<' then
+                print_buku(list[i]);
+                result += 1;
+                end;
+            end;
+        if result = 0 then writeln('Tidak ada buku dalam kategori ini.');
+        stop := True;
+        end
+
+    else if kategori = '<' then
+        begin
+        for i := 1 to nmax do 
+            begin
+            if list[i].tahun_terbit < tahuna then
                 begin
-                    for i := 1 to nmax do 
-                            begin
-                                if list[i].tahun < tahuna then
-                                    begin
-                                        print_buku(list[i]);
-                                        result += 1;
-                                    end;
-                            end;
-                        if result = 0 then writeln('Tidak ada buku dalam kategori ini.');  
-                end
-            else if kategori = '>' then
+                print_buku(list[i]);
+                result += 1;
+                end;
+            end;
+        if result = 0 then writeln('Tidak ada buku dalam kategori ini.');  
+        stop := True;
+        end
+
+    else if kategori = '>' then
+        begin
+        for i := 1 to nmax do 
+            begin
+            if list[i].tahun_terbit > tahuna then
                 begin
-                    for i := 1 to nmax do 
-                            begin
-                                if list[i].tahun > tahuna then
-                                    begin
-                                        print_buku(list[i]);
-                                        result += 1;
-                                    end;
-                            end;
-                        if result = 0 then writeln('Tidak ada buku dalam kategori ini.');  
-                end
-            else if kategori = '>=' then
+                print_buku(list[i]);
+                result += 1;
+                end;
+            end;
+        if result = 0 then writeln('Tidak ada buku dalam kategori ini.'); 
+        stop := True; 
+        end
+
+    else if kategori = '>=' then
+        begin
+        for i := 1 to nmax do 
+            begin
+            if list[i].tahun_terbit >= tahuna then
                 begin
-                    for i := 1 to nmax do 
-                            begin
-                                if list[i].tahun >= tahuna then
-                                    begin
-                                        print_buku(list[i]);
-                                        result += 1;
-                                    end;
-                            end;
-                        if result = 0 then writeln('Tidak ada buku dalam kategori ini.');  
-                end
-            else if kategori = '<=' then
+                print_buku(list[i]);
+                result += 1;
+                end;
+            end;
+        if result = 0 then writeln('Tidak ada buku dalam kategori ini.');  
+        stop := True;
+        end
+
+    else if kategori = '<=' then
+        begin
+            for i := 1 to nmax do 
                 begin
-                    for i := 1 to nmax do 
-                            begin
-                                if list[i].tahun <= tahuna then
-                                    begin
-                                        print_buku(list[i]);
-                                        result += 1;
-                                    end;
-                            end;
-                        if result = 0 then writeln('Tidak ada buku dalam kategori ini.');  
-                end
-        end;
+                if list[i].tahun_terbit <= tahuna then
+                    begin
+                    print_buku(list[i]);
+                    result += 1;
+                    end;
+                end;
+        if result = 0 then writeln('Tidak ada buku dalam kategori ini.'); 
+        stop := True; 
+        end
+    
+    end;
 end.
