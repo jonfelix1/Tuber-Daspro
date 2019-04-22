c68b7b8924f0a541d083969d52ce52a8b18efefc
jonfelix1 <49271261+jonfelix1@users.noreply.github.com>
2019-04-22 21:10:25 +0700

    Add files via upload

--- F05.pas
+++ F05.pas
@@ -1,6 +1,121 @@
-{-diasumsikan id buku dan tanggal yang dimasukkan valid. Namun, bisa saja buku
-yang ingin dipinjam sedang habis (out of stock).-}
-procedure Peminjaman_buku;
+program F05;
+uses sysutils;
+//Kamus Global
+
+const
+    nmax = 100;
+
+type 
+    buku = record
+        id_buku: longint;
+        judul_buku : string;
+        author: string;
+        kategori: string;
+        tahun_terbit : integer;
+        jumlah_buku : integer;
+    end;
+
+    tanggal = record
+        dd, mm, yyyy : integer;
+    end;
+
+    history_peminjaman = record
+        username : string;
+        id_buku : integer;
+        tanggal_peminjaman : tanggal;
+        tanggal_batas_pengembalian : tanggal;
+        status_pengembalian : boolean;
+    end;
+
+    d1 = record
+        arr : array [1..nmax] of buku;
+        neff : integer;
+    end;
+
+    d3 = record
+        arr : array[1..nmax] of history_peminjaman;
+        neff : integer;
+    end;
+
+var 
+    data1 : d1;
+    data3 : d3;
+
+//Prosedurnya
+
+procedure pinjam_buku;
+//Kamus Lokal
+var
+    stock, i, temp_id, ec: integer;
+    found : boolean;
+    temp_tanggal, judul_buku : string;
+
+//Algoritma
 begin
-  
+write('Masukan id buku yang ingin dipinjam: ');
+readln(temp_id);
+write('Masukan tanggal hari ini: ');
+readln(temp_tanggal);
+
+i := 1;
+found := false;
+
+//Mencari data di buku.csv
+while not(found) and (i <= data1.neff) do 
+    begin
+    if data1.arr[i].id_buku = temp_id then
+        begin
+        found := true;
+        stock := data1.arr[i].jumlah_buku;
+        judul_buku := data1.arr[i].judul_buku;
+        end
+    else i += 1;
+    end;
+
+if stock > 0 then
+    begin
+    writeln('Buku ' + judul_buku + ' berhasil dipinjam!');
+    writeln('Tersisa ' + IntToStr(stock-1) + ' buku ' + judul_buku);
+    writeln('Terima kasih sudah meminjam.');
+    data1.arr[i].jumlah_buku -= 1;
+    data3.neff += 1;
+    data3.arr[data3.neff].id_buku := temp_id;
+    val(temp_tanggal[1] +  temp_tanggal[2], data3.arr[i].tanggal_peminjaman.dd, ec);
+    val(temp_tanggal[4] +  temp_tanggal[5], data3.arr[i].tanggal_peminjaman.mm, ec);
+    val(temp_tanggal[7] +  temp_tanggal[8] + temp_tanggal[9] + temp_tanggal[10], data3.arr[i].tanggal_peminjaman.yyyy, ec);
+    end
+
+else
+    begin
+    writeln('Buku ' + judul_buku + ' sedang habis');
+    writeln('Coba lain kali');
+    end;
+
 end;
+
+//Tes Jalan
+begin
+with data1.arr[1] do
+    begin
+    id_buku := 2516;
+    judul_buku := 'Clean Code';
+    author := 'Robert';
+    kategori := 'programming';
+    tahun_terbit := 2010;
+    jumlah_buku := 0
+    end;
+    
+with data1.arr[2] do
+    begin
+    id_buku := 2356;
+    judul_buku := 'Competitive Programming 3';
+    author := 'Steven';
+    kategori := 'programming';
+    tahun_terbit := 2011;
+    jumlah_buku := 1
+    end;
+data1.neff := 2;
+data3.neff := 0;
+pinjam_buku;
+
+end.
